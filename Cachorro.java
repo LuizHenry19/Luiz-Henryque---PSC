f07d9d9827c4e8b13a761d77c018a4e60982138f
LuizHenry19 <luiz.henryque@hotmail.com>
2024-11-21 00:26:52 -0300

    Lista 7 - Exercicio 3

---
+++ Cachorro.java
@@ -0,0 +1,9 @@
+public class Cachorro implements Animal
+{  public void fazerSom() 
+   {  System.out.println("AU au!"); 
+   }
+   
+   public void mover() 
+   {  System.out.println("O cachorro está correndo."); 
+   }
+}
---
+++ TesteAnimal.java
@@ -0,0 +1,10 @@
+public class TesteAnimal {
+    public static void main(String[] args) {
+        
+        Animal meuCachorro = new Cachorro();
+        
+        
+        meuCachorro.fazerSom();  
+        meuCachorro.mover();      
+    }
+}
